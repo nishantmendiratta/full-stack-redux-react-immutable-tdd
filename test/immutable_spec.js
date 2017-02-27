c03cca91e68905b7fc0c04a79dd55dfbec34bc84
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2017-02-27 23:56:45 +0530

    using List from immutable

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,5 @@
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('immutablity', () => {
 	
@@ -17,5 +18,27 @@ describe('immutablity', () => {
 		});
 
 		//more its can go here 
+	});
+
+	describe('A List', () => {
+		function addMovie(currentState, movie) {
+			return currentState.push(movie);
+		}
+
+		it('is immutable', () => {
+			let state = List.of('Trainspotting', '28 Days Later');
+			let nextState = addMovie(state, 'Sunshine');
+
+			expect(nextState).to.equal(List.of(
+				'Trainspotting',
+				'28 Days Later',
+				'Sunshine'
+			));
+
+			expect(state).to.equal(List.of(
+				'Trainspotting',
+				'28 Days Later'
+			));
+		});
 	});
 });
